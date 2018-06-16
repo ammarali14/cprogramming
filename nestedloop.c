f487f770a5f67b22bb4d56a5f4c282ec2f578df6
ammarali14 <40315259+ammarali14@users.noreply.github.com>
2018-06-16 14:27:31 +0500

    Update nestedloop.c
    added antidiagonal

--- nestedloop.c
+++ nestedloop.c
@@ -9,7 +9,7 @@ int main()
     
     for(i = 0; i < size; i++){
         for(j = 0 ; j < size; j++){
-            if(i == j)
+            if(i == j || size -j == i)
                 printf("*");        
         }
         printf("\n");
