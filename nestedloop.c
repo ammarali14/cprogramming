0e12f8fd3cc7f5558e07d3ed2dccca3204e62ce2
Ammar Ali <ammar4it3@gmail.com>
2018-06-16 14:22:19 +0500

    add files

---
+++ nestedloop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int size = 0;
+    int i, j = 0;
+    scanf("Enter size = %d", &size);
+    
+    for(i = 0; i < size; i++){
+        for(j = 0 ; j < size; j++){
+            printf("*");        
+        }
+        printf("\n");
+    }
+    return 0;
+}
